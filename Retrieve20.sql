e15330a9fe041e20bf323db80ae80a7b811ef3fe
Ayan Chowdhury <ayanchowdhury901@gmail.com>
2023-11-20 10:36:32 -0800

    Add files via upload

---
+++ Retrieve20.sql
@@ -0,0 +1,7 @@
+-- write a SQL query to find the Nobel Prize winners in 1970 excluding the subjects of 
+-- Physiology and Economics. Return year, subject, winner, country, and category.
+
+SELECT *
+ FROM nobel_win 
+   WHERE year=1970
+    AND subject NOT IN('Physiology','Economics');
---
+++ Retrieve22.sql
@@ -0,0 +1,5 @@
+-- write a SQL query to find the details of the Nobel Prize winner
+--'Johannes Georg Bednorz'. 
+--Return year, subject, winner, country, and category. 
+
+SELECT * FROM nobel_win WHERE winner='Johannes Georg Bednorz';
